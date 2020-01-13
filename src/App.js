edbb35c9c0cb6cd0beae7d69b17dfd2e4cd99541
modefy <18553485354@163.com>
2020-01-13 18:52:59 +0800

    修改文件

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Button, message } from 'antd';
+
+/*
+应用的根组件
+*/
+export default class App extends Component {
+    render() {
+        return <Button type="primary" onClick={ this.handleClick }>Primary</Button>
+    }
+    handleClick = () => {
+        message.success('sucess');
+    }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// 入口文件
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+import App from './App'
+
+// 将App组件标签渲染到index页面的div容器中
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
