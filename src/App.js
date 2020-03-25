eac333e8d5e81f779a1447b7470cc9c196fc32d3
modefy <18553485354@163.com>
2020-03-25 08:58:44 +0800

    3.24提交

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch  } from 'react-router-dom'
 
-import { Button, message } from 'antd';
+// 引入组件
+import Login from './pages/login/Login'
+import Admin from './pages/admin/Admin'
 
 /*
 应用的根组件
 */
 export default class App extends Component {
     render() {
-        return <Button type="primary" onClick={ this.handleClick }>Primary</Button>
-    }
-    handleClick = () => {
-        message.success('success!');
+        return (
+            <BrowserRouter>
+                <Switch>{/* 只匹配其中一个 */}
+                    <Route path="/login" component={Login}></Route>
+                    <Route path="/" component={Admin}></Route>
+                </Switch>
+            </BrowserRouter>
+        );
     }
 }
+
---
+++ src/api/index.js
@@ -0,0 +1,15 @@
+/**
+ * 要求：能根据接口文档定义接口请求函数
+ * 包含应用中所有接口请求函数的模块
+ * 每个函数的返回值都是promise
+ */
+import ajax from './ajax.js'
+
+//登录
+// export function reqLogin(username, password) {
+//     return ajax('./login', { username, password }, 'POST')
+// }
+export const reqLogin = (username, password) => ajax('./login', { username, password }, 'POST')
+
+// 添加用户(url是端口号后面的地址)
+export const reqAddUser = (user) => ajax('/add', user, 'POST')
